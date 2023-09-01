ac2a5b89f4b5e0c891227006b73a27cbdcc9be09
amirvalie <hosseinvalie@gmail.com>
2023-09-01 19:03:35 +0330

    Add feature:add UploadImage model to store images uploaded from users

--- image_app/models.py
+++ image_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UploadImage(models.Model):
+    image = models.ImageField(upload_to='image')
+
+    def __str__(self) -> str:
+        return self.image.name
