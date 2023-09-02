ff9fd8f07dc9b7fd20240af3c60e88b8324f8c6a
amirvalie <hosseinvalie@gmail.com>
2023-09-02 20:20:50 +0330

    Fixe bug:rename the app config

--- config/celery.py
+++ config/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_celery.settings")
-app = Celery("django_celery")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+app = Celery("app")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
