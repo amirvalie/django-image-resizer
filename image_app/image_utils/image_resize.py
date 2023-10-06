8ec0faee5803692cbba4f63b40f155bbff71f093
amirvalie <hosseinvalie@gmail.com>
2023-10-06 20:25:19 +0330

    Update feature:Instead of using protocol and dataclass decorator we use abstract.all the classed from the BaseImage should enforce to override create_image_file and save methods

--- image_app/image_utils/image_resize.py
+++ image_app/image_utils/image_resize.py
@@ -1,13 +1,13 @@
-from dataclasses import dataclass
+from abc import ABC
 from io import BytesIO
-from typing import ClassVar, Protocol
 from django.core.files.base import ContentFile
 from django.db.models.fields.files import ImageFieldFile
 from PIL import Image
 
-@dataclass
-class BaseImage(Protocol):
-    image: Image.Image
+
+class BaseImage(ABC):
+    def __init__(self, image: Image.Image) -> None:
+        self.image = image
 
     def create_image_file(self) -> ContentFile:
         ...
@@ -18,50 +18,51 @@ class BaseImage(Protocol):
 
 def open_image(image: ImageFieldFile) -> Image.Image:
     try:
-        image = Image.open(image) # type: ignore
-        return image # type: ignore
+        image = Image.open(image)  # type: ignore
+        return image  # type: ignore
     except IOError:
         raise IOError
 
 
 def target_file_name(image_name: str) -> str:
-    file_name = image_name.split('/')[-1].split('.')[0]
+    file_name = image_name.split("/")[-1].split(".")[0]
     return file_name
 
+
 class ImageResize:
-    def __init__(self,
-                 image: ImageFieldFile,
-                 width: int,
-                 height: int,
-                 aspect_ratio: bool=True,
-                 ) -> None:
+    def __init__(
+        self,
+        image: ImageFieldFile,
+        width: int,
+        height: int,
+        aspect_ratio: bool = True,
+    ) -> None:
         self.image = open_image(image)
         self.width = width
         self.height = height
         self.aspect_ratio = aspect_ratio
-        self.format= self.image.format
+        self.format = self.image.format
 
     def resize(self) -> ContentFile:
         if self.aspect_ratio == True:
-            self.image.thumbnail((self.width, self.height)) # type: ignore
+            self.image.thumbnail((self.width, self.height))  # type: ignore
             return self.create_image_file()
         else:
-            img = self.image.resize((self.width, self.height)) # type: ignore
+            img = self.image.resize((self.width, self.height))  # type: ignore
         return self.create_image_file(img)
 
-    def create_image_file(self, img:Image.Image | None=None) -> ContentFile:
+    def create_image_file(self, img: Image.Image | None = None) -> ContentFile:
         buffer = BytesIO()
         if img:
-            img.save(buffer, format=self.format) # type: ignore
+            img.save(buffer, format=self.format)  # type: ignore
         else:
-            self.image.save(buffer, format=self.format) # type: ignore
+            self.image.save(buffer, format=self.format)  # type: ignore
         image_file = ContentFile(buffer.getvalue())
         return image_file
 
     def save(self, field: ImageFieldFile) -> None:
         field.save(
-            f'{target_file_name(field.name)}.{self.image.format.lower()}',
+            f"{target_file_name(field.name)}.{self.image.format.lower()}",
             self.resize(),
             save=True,
         )
-
